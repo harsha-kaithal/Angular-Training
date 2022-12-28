f172b0193d83c3b927a35d6534fda22438acc5a6
harsha-kaithal <67072042+harsha-kaithal@users.noreply.github.com>
2022-12-29 00:53:34 +0530

    del

--- Material_FormAssignment_with_Routing_src/app/model/form.ts
+++
@@ -1,9 +0,0 @@
-export class MyForm{
-    name:string='';  
-    email:string='';  
-    password:string='';
-    dob:any='';  
-    gender:string='';  
-    phone:string='';  
-    skills:string='';  
-  }
